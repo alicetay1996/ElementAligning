c71b5a7efb9ed343429b745b495c2cf66e743c3f
AliceTay1996 <alice.tay@pingspace.co>
2021-01-26 14:49:27 +0800

    Newly added

---
+++ alignApp/src/app/savebutton/savebutton.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-savebutton',
+  templateUrl: './savebutton.component.html',
+  styleUrls: ['./savebutton.component.scss']
+})
+export class SavebuttonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
